cb80b5aa46dc033e3b5dac94162447d407d525c5
Muhammed Halil Albayrak <malbayra@k1m03s09.42istanbul.com.tr>
2025-01-24 19:56:00 +0300

    42_minitalk

---
+++ minitalk/minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malbayra <malbayra@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 17:03:34 by malbayra          #+#    #+#             */
+/*   Updated: 2025/01/24 19:40:56 by malbayra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+#define MINITALK_H
+#define _POSIX_C_SOURCE 200809L
+
+#include "libft/libft.h"
+#include "libft/ft_printf/ft_printf.h"
+# include <signal.h>
+# include <unistd.h>
+
+#endif
