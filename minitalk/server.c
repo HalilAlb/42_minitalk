390aa5ce644aad1e2f680765e4310be2aa441e11
Muhammed Halil Albayrak <malbayra@k1m03s09.42istanbul.com.tr>
2025-01-27 16:58:43 +0300

    last update

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: malbayra <malbayra@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 17:01:31 by malbayra          #+#    #+#             */
-/*   Updated: 2025/01/27 14:01:20 by malbayra         ###   ########.fr       */
+/*   Updated: 2025/01/27 15:42:28 by malbayra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ void	ft_error(const char *msg)
 
 void	ft_handler(int sig, siginfo_t *info, void *context)
 {
-	static unsigned char	c = 0;
-	static int				bit = 0;
+	static unsigned char	c;
+	static int				bit;
 
 	(void)context;
 	c |= (sig == SIGUSR2) << bit++;
